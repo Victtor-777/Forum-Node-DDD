9def5c246bcdfe7e899ce93b2b7d3e63a47b04a0
Victtor777 <victtor.eng.software@gmail.com>
2024-08-19 17:03:10 -0300

    refactoring folders and add create question use case

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,6 +1,6 @@
-import { AnswerQuestionUseCase } from "./answer-question";
+import { Answer } from "../../enterprise/entities/answer";
 import { AnswerRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { AnswerQuestionUseCase } from "./answer-question";
 
 const fakeAnswersRepository: AnswerRepository = {
   create: async (answer: Answer) => {
