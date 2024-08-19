c9306e34825fa67b1110e0e7821f7c6e187857cb
Victtor777 <victtor.eng.software@gmail.com>
2024-08-19 18:18:49 -0300

    generating fictitious data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,24 @@
+import { faker } from "@faker-js/faker";
+
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import {
   Question,
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: "Example question",
-    slug: Slug.create("example-question"),
-    authorId: new UniqueEntityId(),
-    content: "Example content",
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  );
 
   return question;
 }
